c45401f9b131c73a9470d2c476b2cfdf04b8cac4
Michael Jeung <michaelj229@gmail.com>
2023-04-12 18:32:08 -0400

    tests do not pass. functionality is correct

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Item({ name, category }) {
+function Item({ name, category, }) {
+  const [inCart, setInCart] = useState(false)
+  function wichael(){
+    setInCart(!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={!inCart ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={wichael}>{!inCart ? "add to cart" : "in-cart"}</button>
     </li>
   );
 }
